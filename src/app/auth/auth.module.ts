a64ea27b547ff3cc8c1a65b6adb0488f6f6670f5
Devin Shoemaker <devinshoe@gmail.com>
2020-01-05 10:54:32 -0600

    auth: add FirebaseUIComponent to module declarations

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,15 +1,13 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
-
 import { AuthPageRoutingModule } from './auth-routing.module';
-
 import { AuthPage } from './auth.page';
+import { FirebaseUIComponent } from './firebase-ui/firebase-ui.component';
 
 @NgModule({
   imports: [CommonModule, FormsModule, IonicModule, AuthPageRoutingModule],
-  declarations: [AuthPage]
+  declarations: [AuthPage, FirebaseUIComponent]
 })
 export class AuthPageModule {}
