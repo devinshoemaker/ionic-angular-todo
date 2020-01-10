8bd3980c61a2c507a02c57bf5a2aefc26347b6aa
Devin Shoemaker <devinshoe@gmail.com>
2020-01-09 21:10:03 -0600

    [WIP] auth/AuthPage: display FirebaseUI

--- src/app/auth/auth.page.ts
+++ src/app/auth/auth.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@anuglar/fire/auth';
+import * as firebase from 'firebase';
+import * as firebaseui from 'firebaseui';
 
 @Component({
   selector: 'app-auth',
@@ -6,7 +9,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./auth.page.scss']
 })
 export class AuthPage implements OnInit {
-  constructor() {}
+  // FirebaseUI config.
+  public uiConfig: firebaseui.auth.Config = {
+    signInSuccessUrl: '/home',
+    signInOptions: [
+      // Leave the lines as is for the providers you want to offer your users.
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+      firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+      firebase.auth.GithubAuthProvider.PROVIDER_ID,
+      firebase.auth.EmailAuthProvider.PROVIDER_ID,
+      firebase.auth.PhoneAuthProvider.PROVIDER_ID,
+      firebaseui.auth.AnonymousAuthProvider.PROVIDER_ID
+    ],
+    // tosUrl and privacyPolicyUrl accept either url string or a callback
+    // function.
+    // Terms of service url/callback.
+    tosUrl: '<your-tos-url>',
+    // Privacy policy url/callback.
+    privacyPolicyUrl: function() {
+      window.location.assign('<your-privacy-policy-url>');
+    }
+  };
+
+  constructor(public afAuth: AngularFireAuth) {}
 
   ngOnInit() {}
 }
